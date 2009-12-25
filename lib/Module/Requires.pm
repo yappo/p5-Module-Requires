8705f57fdfcc34d8a7ecc4669e3a014533e865d4
yappo <yappo@shibuya.pl>
2009-12-25 10:51:12 +0900

    initial commit

---
+++ lib/Module/Requires.pm
@@ -0,0 +1,32 @@
+package Module::Requires;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Module::Requires -
+
+=head1 SYNOPSIS
+
+  use Module::Requires;
+
+=head1 DESCRIPTION
+
+Module::Requires is
+
+=head1 AUTHOR
+
+Default Name E<lt>default {at} example.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
